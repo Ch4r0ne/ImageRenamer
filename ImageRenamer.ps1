7ea127cc86edcdb4d9671612b72ac6797a0531ab
Ch4r0ne <54702161+Ch4r0ne@users.noreply.github.com>
2022-12-18 19:44:55 +0100

    Update ImageRenamer.ps1

--- ImageRenamer.ps1
+++ ImageRenamer.ps1
@@ -5,33 +5,37 @@
 # Example path: "C:\Users\Username\Documents\ImageFolder".
 $folderPath = "C:\Users\Username\Documents\ImageFolder"
 
-# Get all JPG files in the folder
-$imageFiles = Get-ChildItem -Path $folderPath -Filter "*.jpg"
+# List of supported image file extensions
+$supportedFileTypes = @('.jpg', '.jpeg', '.png')
 
-foreach ($imageFile in $imageFiles) {
-  # Open image file
-  $image = [System.Drawing.Image]::FromFile($imageFile.FullName)
+# Iterate through all files in the specified folder
+Get-ChildItem -Path $folderPath -Filter "*.*" | ForEach-Object {
+  # Check if the file has a supported file extension
+  if ($supportedFileTypes -contains $_.Extension) {
+    # Open image file
+    $image = [System.Drawing.Image]::FromFile($_.FullName)
 
-  # Check if metadata is present
-  if ($image.PropertyIdList.Contains(36867)) {
-    # Read date taken
-    $dateTaken = $image.GetPropertyItem(36867).Value
+    # Check if metadata is present
+    if ($image.PropertyIdList.Contains(36867)) {
+      # Read date taken
+      $dateTaken = $image.GetPropertyItem(36867).Value
 
-    # Format date taken and use as new file name
-    $newFileName = [System.Text.Encoding]::ASCII.GetString($dateTaken).Replace(":", "-").Replace(" ", "_") + $imageFile.Extension
-    $newFileName = $newFileName -replace '[^\w\.\-]', ''  # Do not allow special characters in file name
-  }
-  else {
-    Write-Output "The image file $($imageFile.Name) does not contain metadata with the date taken and was not renamed."
-    $newFileName = $null  # No valid new file name present
-  }
+      # Format date taken and use as new file name
+      $newFileName = [System.Text.Encoding]::ASCII.GetString($dateTaken).Replace(":", "-").Replace(" ", "_") + $_.Extension
+      $newFileName = $newFileName -replace '[^\w\.\-]', ''
+    }
+    else {
+      Write-Output "The image file $($_.Name) does not contain metadata with the date taken and was not renamed."
+      $newFileName = $null  # No valid new file name present
+    }
 
-  # Close image file
-  $image.Dispose()
+    # Close image file
+    $image.Dispose()
 
-  # Rename image file if a valid new file name is present
-  if ($newFileName) {
-    Rename-Item $imageFile.FullName -NewName $newFileName
-    Write-Output "Image file $($imageFile.Name) was renamed to $newFileName."
+    # Rename image file if a valid new file name is present
+    if ($newFileName) {
+      Rename-Item $_.FullName -NewName $newFileName
+      Write-Output "Image file $($_.Name) was renamed to $newFileName."
+    }
   }
 }
